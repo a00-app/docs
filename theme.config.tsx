a23f89bb51292685503933e9828b71e8df138532
Ozan Andaç <oynozan@hotmail.com>
2025-10-12 16:04:22 +0200

    documentation v0.1

---
+++ theme.config.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>a00</span>,
+  project: {
+    link: 'https://github.com/a00-app',
+  },
+  footer: {
+    text: 'Powered by U2U Network and IPFS',
+  },
+}
+
+export default config
